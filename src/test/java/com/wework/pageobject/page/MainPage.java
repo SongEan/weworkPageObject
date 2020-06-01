1a3385cc8241aac8921f4c732b94b2388a3c38cf
SongEan <xchanmao5966@yeah.net>
2020-06-01 16:06:57 +0800

    新增util

---
+++ src/test/java/com/wework/pageobject/page/MainPage.java
@@ -0,0 +1,11 @@
+package com.wework.pageobject.page;
+
+/**
+ * Created by IntelliJ Idea IDEA
+ * java version "1.8.0_91"
+ * Author: Ean Song
+ * Date: 2020-06-01
+ * Time: 15:26
+ */
+public class MainPage {
+}
